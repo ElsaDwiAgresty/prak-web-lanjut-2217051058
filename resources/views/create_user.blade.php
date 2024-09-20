c26fbb7af30f74cd349740f25a3d52476ffd9636
ElsaDwiAgresty <165319077+ElsaDwiAgresty@users.noreply.github.com>
2024-09-20 20:45:46 +0700

    commit tugas 3 prak-web-lanjut

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -1,24 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Create User</title>
 </head>
+
 <body>
-    <h1>Ini Halaman Create User</h1>
-    <form action="{{ route('user.store') }}" method="POST">
+    <h1 class="text-center text-2xl mb-6 font-bold mt-4">Isi Biodata</h1>
+    <form action="{{ route('user.store') }}" method="POST" class="flex justify-center">
         @csrf
-        <label for="nama">Nama</label>
-        <input type="text" id="nama" name="nama"><br>
+        <div class="bg-slate-400 h-60 w-96 p-6 rounded-lg">
+            <div class="mt-4 flex items-center">
+                <label for="nama" class="w-1/3 text-white font-bold text-lg">Nama : </label>
+                <input type="text" id="nama" name="nama" class="rounded-lg h-8 w-full px-2">
+            </div>
 
-        <label for="npm">NPM</label>
-        <input type="text" id="npm" name="npm"><br><br>
+            <div class="mt-4 flex items-center">
+                <label for="npm" class="w-1/3 text-white font-bold text-lg">NPM : </label>
+                <input type="text" id="npm" name="npm" class="rounded-lg h-8 w-full px-2">
+            </div>
 
-        <label for="kelas">Kelas</label>
-        <input type="text" id="kelas" name="npm"><br><br>
+            <div class="mt-4 flex items-center">
+                <label for="kelas" class="w-1/3 text-white font-bold text-lg">Kelas : </label>
+                <input type="text" id="kelas" name="kelas" class="rounded-lg h-8 w-full px-2">
+            </div>
 
-        <input type="submit" value="submit">
+            <div class="mt-6 flex justify-end">
+                <input type="submit" value="Submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-600">
+            </div>
+        </div>
     </form>
 </body>
+
 </html>
