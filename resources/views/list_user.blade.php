2479bba00b791af7f43c27eea9dfbe7f9cfd2027
ElsaDwiAgresty <165319077+ElsaDwiAgresty@users.noreply.github.com>
2024-10-24 10:13:46 +0700

    Commit Modul 8

--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -25,14 +25,14 @@
                         <td>{{ $user['nama_kelas'] }}</td>
                         <td>
                             @if($user['foto'])
-                                <img src="{{ asset($user['foto']) }}" alt="User Photo" width="100" >
+                                <img src="{{ asset('storage/uploads/'. $user['foto']) }}" alt="User Photo" width="100" >
                             @endif
                         </td>
                         <td>
                             <div class="d-flex justify-content-center">
-                                <a href="" class="btn btn-primary btn-sm">View</a>
-                                <a href="" class="btn btn-sm btn-warning mx-2">Edit</a>
-                                <form action="" method="POST" style="display:inline-block;">
+                                <a href="{{ route('user.show', $user['id'])}}" class="btn btn-primary btn-sm">View</a>
+                                <a href="{{ route('user.edit', $user['id']) }}" class="btn btn-sm btn-warning mx-2">Edit</a>
+                                <form action="{{ route('user.destroy', $user['id']) }}" method="POST" style="display:inline-block;">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-sm btn-danger mx-0.5" onclick="return confirm('Apakah Anda yakin ingin menghapus pengguna ini?')">Delete</button>
